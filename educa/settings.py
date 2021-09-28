8ac9850de7696a779f0887a3b21101c4a7fc705e
AntonEsipow <esipow.anton99@gmail.com>
2021-09-28 22:23:25 +0300

    Add models for courses app

--- educa/settings.py
+++ educa/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'courses.apps.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
